906836745d11b59800e9d836a6ead8e4e5ebd31f
가경건 <115058471+gagyounggun@users.noreply.github.com>
2025-09-01 15:03:32 +0900

    좀 돼라!

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <BrowserRouter >
+    <BrowserRouter basename='react-netflix' >
       <App />
     </BrowserRouter>,
     document.getElementById('root')
