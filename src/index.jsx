e489b77ecb0b904e79431236d42b0a07ec4ecc5b
gagyounggun <ccc00311@naver.com>
2025-09-03 16:24:42 +0900

    w

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <BrowserRouter basename='react-netflix-clone' >
+    <BrowserRouter basename='react-netflix' >
       <App />
     </BrowserRouter>,
     document.getElementById('root')
