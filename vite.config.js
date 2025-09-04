1930984f2cab1d9255df828ebf710c38e345f58f
gagyounggun <ccc00311@naver.com>
2025-09-04 17:10:33 +0900

    w

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-netflix-clone/' // 이 부분을 추가하거나 수정하세요
+  base: '/react-netflix/' // 이 부분을 추가하거나 수정하세요
 })
